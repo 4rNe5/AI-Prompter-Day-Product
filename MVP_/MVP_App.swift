669c241cfe1d4c4c69a766021b247949ce3b4cf7
박준현 <me@4rne5.dev>
2023-08-27 23:22:29 +0900

    Initial Commit

---
+++ MVP_/MVP_App.swift
@@ -0,0 +1,17 @@
+//
+//  MVP_App.swift
+//  MVP_
+//
+//  Created by 4rNe5 on 2023/08/27.
+//
+
+import SwiftUI
+
+@main
+struct MVP_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
