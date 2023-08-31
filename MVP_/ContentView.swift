3a80ff9eb32490ac4f65fa81d4f9245e0a980e12
박준현 <me@4rne5.dev>
2023-08-31 23:14:01 +0900

    Good

--- MVP_/ContentView.swift
+++ MVP_/ContentView.swift
@@ -8,14 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    @State var idText: String = ""
+
+    @State var pwText: String = ""
+
     var body: some View {
+
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+
+            Image(systemName: "rectangle.on.rectangle.circle")
+                .resizable()
+                .frame(width: 150, height: 150)
+                .foregroundColor(.black.opacity(0.85))
+            
+            Text("노트정리를 바꾸다.")
+                .font(.system(size: 20, weight: .semibold))
+                .foregroundColor(.black.opacity(0.85))
+            
+            Text("\(Text("N/O").foregroundColor(Color.purple))Write")
+                .font(.system(size: 50, weight: .semibold))
+                .foregroundColor(.black.opacity(0.85))
+                .padding(.bottom,40)
+            HStack {
+
+                Image(systemName: "exclamationmark.lock.fill")
+                    .padding(.leading, 10)
+
+                TextField("ID", text: $idText)
+                    .bold()
+            }
+            .frame(width: 600, height: 60)
+            .background(.gray.opacity(0.15))
+            .cornerRadius(8)
+            .padding(.horizontal, 20)
+            .padding(5)
+
+            HStack {
+
+                Image(systemName: "exclamationmark.lock.fill")
+                    .padding(.leading, 10)
+
+                SecureField("PW", text: $pwText)
+                    .bold()
+            }
+            .frame(width: 600, height: 60)
+            .background(.gray.opacity(0.15))
+            .cornerRadius(8)
+            .padding(.horizontal, 20)
+
         }
-        .padding()
     }
 }
 
@@ -24,3 +66,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
